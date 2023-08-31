56f02c031163ea4a2260a6079c46d2cb197a8ea1
Jhon-sants <jonathanprog13@gmail.com>
2023-08-31 11:11:21 -0300

    código do primeiro projeto java no bootcamp

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//Código do aluno Jonathan Nascimento Santos.
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite o seu nome completo: ");
+        String nomeCliente = entrada.next();
+
+        System.out.print("Número de usuário: ");
+        int numUsuario = entrada.nextInt();
+
+        System.out.print("Agência: ");
+        String numAgencia = entrada.next();
+
+        System.out.print("Valor de depósito: ");
+        double saldo = entrada.nextDouble();
+
+        entrada.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + numAgencia + ", conta " + numUsuario + ". Seu saldo de " + saldo
+                + " reais já está disponível para saque.");
+
     }
 }
